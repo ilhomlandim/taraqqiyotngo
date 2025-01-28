d562dc60e38567498333d7fea0465db36a3220c0
mirzoboxu <mirzoulugbekkhudoyberdiyev@gmail.com>
2025-01-28 16:23:51 +0500

    Refactor not-found

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function layout({ children }) {
   return (
     <html>
--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,18 +1,5 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function NotFound() {
-  return (
-    <section className="h-full py-14">
-      <div className="base-container">
-        <div className="flex flex-col items-center text-center">
-          <h1 className="text-5xl font-bold mb-5">404</h1>
-          <p>Bunday sahifa mavjud emas</p>
-          <Button asChild>
-            <Link href="/">Bosh sahifa</Link>
-          </Button>
-        </div>
-      </div>
-    </section>
-  );
+  redirect("/");
 }
